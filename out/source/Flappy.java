84a4d1a92e7dc939054e0b28a02d1d73ade8dc60
LifesMystery <arjunvsudn@gmail.com>
2022-04-14 08:54:41 -0500

    beforeSubmit

--- out/source/Flappy.java
+++ out/source/Flappy.java
@@ -170,64 +170,63 @@ public void backgroundDraw()
         lightningTrigger1 = (int)random(100);
         largeLightning.DisplayStatic(200,620);
     }  
-        if (smallLightning.Running() ==  false)
+    if (smallLightning.Running() ==  false)
+    {
+        lightningTrigger2 = (int)random(100);
+        smallLightning.DisplayStatic(1700,680);
+    }
+            
+    if (distantLightning.Running() ==  false)
+    {
+        lightningTrigger3 = (int)random(100);
+        distantLightning.DisplayStatic(815,475);
+    }
+    if (bubbleMaker.Running() ==  false)
+    {
+        lightningTrigger4 = (int)random(100);
+        bubbleMaker.DisplayStatic(1100,800);
+    }
+    if (bubbleMaker2.Running() ==  false)
+    {
+        lightningTrigger5 = (int)random(70);
+        bubbleMaker2.DisplayStatic(1080,790);
+    }
+    if (bubbleMaker3.Running() ==  false)
+    {
+        lightningTrigger6 = (int)random(90);
+        bubbleMaker3.DisplayStatic(1120,790);
+    }
+            
+            
+    if (lightningTrigger1 ==1)
+    {
+        largeLightning.DisplayAnimation(200,620,6);
+    }
+            
+    if (lightningTrigger2 ==  2)
         {
-            lightningTrigger2 = (int)random(100);
-            smallLightning.DisplayStatic(1700,680);
+            smallLightning.DisplayAnimation(1700,680,6);
         }
             
-            if (distantLightning.Running() ==  false)
-            {
-                lightningTrigger3 = (int)random(100);
-                distantLightning.DisplayStatic(815,475);
-            }
-                if (bubbleMaker.Running() ==  false)
-                {
-                    lightningTrigger4 = (int)random(100);
-                    bubbleMaker.DisplayStatic(1100,800);
-                }
-                    if (bubbleMaker2.Running() ==  false)
-                    {
-                        lightningTrigger5 = (int)random(70);
-                        bubbleMaker2.DisplayStatic(1080,790);
-                    }
-                        if (bubbleMaker3.Running() ==  false)
-                        {
-                            lightningTrigger6 = (int)random(90);
-                            bubbleMaker3.DisplayStatic(1120,790);
-                        }
-                            
-                            
-                            if (lightningTrigger1 ==1)
-                            {
-                                largeLightning.DisplayAnimation(200,620,6);
-                            }
-                                
-                              if (lightningTrigger2 ==  2)
-                                {
-                                    smallLightning.DisplayAnimation(1700,680,6);
-                                }
-                                    
-                                  if (lightningTrigger3 ==  3)
-                                {
-                                        distantLightning.DisplayAnimation(815,475,6);
-                                    }
-                                        
-                                      if (lightningTrigger4 ==  4)
-                                    {
-                                        bubbleMaker.DisplayAnimation(1100,800,4);
-                                    }
-                                      if (lightningTrigger5 ==  5)
-                                    {
-                                        bubbleMaker2.DisplayAnimation(1080,790,4);
-                                    }
-                                      if (lightningTrigger6 ==  6)
-                                    {
-                                        bubbleMaker3.DisplayAnimation(1120,790,4);
-                                    }
-                                        
-                                    }
-                                        
+    if (lightningTrigger3 ==  3)
+    {
+        distantLightning.DisplayAnimation(815,475,6);
+    }
+    if (lightningTrigger4 ==  4)
+    {
+        bubbleMaker.DisplayAnimation(1100,800,4);
+    }
+    if (lightningTrigger5 ==  5)
+    {
+        bubbleMaker2.DisplayAnimation(1080,790,4);
+    }
+    if (lightningTrigger6 ==  6)
+    {
+        bubbleMaker3.DisplayAnimation(1120,790,4);
+    }
+        
+    }
+        
 
 class BackgroundAnimation
 {
